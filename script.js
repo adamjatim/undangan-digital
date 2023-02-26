2595a1fa9bdf051357157a7049f2699b06a3f9e8
adamjatim <adamrahmatilahi@gmail.com>
2023-02-26 07:04:51 +0700

    update add music

--- script.js
+++ script.js
@@ -24,53 +24,22 @@
         }, 0)
 }());
 
-var audio = document.getElementById("myAudio");
-audio.muted = false;
+const audio = document.getElementById('my-audio');
+const playButton = document.getElementById('play-button');
+const pauseButton = document.getElementById('pause-button');
 
-// Check if the audio is playing
-if(audio.paused) {
-    audio.play();
+function playAudio() {
+  audio.play();
+  playButton.style.display = 'none';
+  pauseButton.style.display = 'inline';
 }
 
-function showAudioPermissionPopup() {
-    const popup = document.createElement('div');
-    popup.innerHTML = 'Allow audio autoplay to listen to our content';
-    popup.style.position = 'fixed';
-    popup.style.bottom = '20px';
-    popup.style.right = '20px';
-    popup.style.backgroundColor = '#f5f5f5';
-    popup.style.border = '1px solid #ccc';
-    popup.style.padding = '10px';
-    popup.style.zIndex = '9999';
-    popup.onclick = () => {
-      requestAudioPermission();
-      popup.remove();
-    };
-    document.body.appendChild(popup);
-  }
-  
-  async function requestAudioPermission() {
-    try {
-      const permissionStatus = await navigator.permissions.query({ name: 'autoplay' });
-      if (permissionStatus.state === 'granted') {
-        playAudio();
-      } else if (permissionStatus.state === 'prompt') {
-        const result = await permissionStatus.ask();
-        if (result === 'granted') {
-          playAudio();
-        }
-      }
-    } catch (error) {
-      console.error(error);
-    }
-  }
-  
-  function playAudio() {
-    const audio = new Audio('./src/audio/BarakaAllahuLakuma.mp3');
-    audio.autoplay = true;
-    audio.loop = true;
-    audio.controls = false;
-    document.body.appendChild(audio);
-  }
-  
-  showAudioPermissionPopup();
+function pauseAudio() {
+  audio.pause();
+  playButton.style.display = 'inline';
+  pauseButton.style.display = 'none';
+}
+
+audio.addEventListener('ended', function() {
+  pauseAudio();
+});
